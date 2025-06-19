d65e707ff250546a451dce805b0e0c38c7e881e2
dquoangvinh <duongquang01649364382@gmail.com>
2025-06-19 19:16:56 +0700

    Update Extra_Assignment_1_2.sql

--- Extra Assignment/Extra_Assignment_1_2.sql
+++ Extra Assignment/Extra_Assignment_1_2.sql
@@ -2,18 +2,37 @@
 CREATE DATABASE FresherManagement;
 USE FresherManagement;
 
--- Tạo table Trainee
+-- Exercise 1:Design a table  
+-- Question 1: Tạo table với các ràng buộc và kiểu dữ liệu 
 CREATE TABLE Trainee (
     TraineeID INT AUTO_INCREMENT PRIMARY KEY,
     Full_Name VARCHAR(100) NOT NULL,
     Birth_Date DATE,
     Gender ENUM('male', 'female', 'unknown') DEFAULT 'unknown',
-    ET_IQ TINYINT UNSIGNED,
-    ET_Gmath TINYINT UNSIGNED,
-    ET_English TINYINT UNSIGNED,
+    ET_IQ TINYINT CHECK (ET_IQ >= 0 AND ET_IQ <= 20),
+    ET_Gmath TINYINT CHECK (ET_Gmath >= 0 AND ET_Gmath <= 20),
+    ET_English TINYINT CHECK (ET_English >= 0 AND ET_English <= 50),
     Training_Class VARCHAR(50),
     Evaluation_Notes TEXT
 );
 
+-- Question 2: thêm trường VTI_Account với điều kiện not null & unique 
 ALTER TABLE Trainee 
-ADD COLUMN VTI_Account VARCHAR(50) NOT NULL UNIQUE;  
+ADD COLUMN VTI_Account VARCHAR(50) NOT NULL UNIQUE;
+
+-- Exercise 2: Data Types 
+CREATE TABLE Exercise2 (
+    ID INT AUTO_INCREMENT PRIMARY KEY, 
+    `Name` VARCHAR(100),                 
+    `Code` CHAR(5),                      
+    ModifiedDate DATETIME             
+);  
+
+-- Exercise 3: Data Types (2) 
+CREATE TABLE Exercise3 (
+    ID INT AUTO_INCREMENT PRIMARY KEY,  
+    `Name` VARCHAR(100),                 
+    BirthDate DATE,                   
+    Gender TINYINT,                   
+    IsDeletedFlag BIT                 
+);
