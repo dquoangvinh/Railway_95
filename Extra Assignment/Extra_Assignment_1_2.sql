cf4ba6355940928a89d0e0439d06c94e4ca42ee4
dquoangvinh <duongquang01649364382@gmail.com>
2025-06-19 14:07:55 +0700

    Add Testing System and Extra Assignment folders at root level

---
+++ Extra Assignment/Extra_Assignment_1_2.sql
@@ -0,0 +1,19 @@
+-- Tạo database
+CREATE DATABASE FresherManagement;
+USE FresherManagement;
+
+-- Tạo table Trainee
+CREATE TABLE Trainee (
+    TraineeID INT AUTO_INCREMENT PRIMARY KEY,
+    Full_Name VARCHAR(100) NOT NULL,
+    Birth_Date DATE,
+    Gender ENUM('male', 'female', 'unknown') DEFAULT 'unknown',
+    ET_IQ TINYINT UNSIGNED,
+    ET_Gmath TINYINT UNSIGNED,
+    ET_English TINYINT UNSIGNED,
+    Training_Class VARCHAR(50),
+    Evaluation_Notes TEXT
+);
+
+ALTER TABLE Trainee 
+ADD COLUMN VTI_Account VARCHAR(50) NOT NULL UNIQUE;  
